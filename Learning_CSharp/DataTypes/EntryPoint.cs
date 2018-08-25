ffc349ad31b721f46f919eead3b31b0a96b44766
Nabeel John <nabeeljohn@outlook.com>
2018-08-24 21:37:24 -0400

    Added new project DataTypes

---
+++ Learning_CSharp/DataTypes/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿class EntryPoint
+{
+    static void Main()
+    {
+        //Convert other data types to String
+
+        int someCrazyNumber = 4536334;
+        System.Console.WriteLine(someCrazyNumber);
+        System.Console.WriteLine(someCrazyNumber.GetType());
+
+        string theNumberWasStringed = someCrazyNumber.ToString();
+        System.Console.WriteLine(theNumberWasStringed);
+        System.Console.WriteLine(theNumberWasStringed.GetType());
+
+        //confirm another way 
+        System.Console.WriteLine(theNumberWasStringed[4]);
+
+        bool theTruth = true;
+        System.Console.WriteLine(theTruth.ToString()[3]);
+
+    }
+}
